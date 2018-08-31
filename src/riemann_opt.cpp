ec434fca9ffe90e8846811b876aa95476df5672e
Alexey Rybakov <rybakov.aax@gmail.com>
2018-08-31 16:28:25 +0300

    clean after guessp vectorization

--- src/riemann_opt.cpp
+++ src/riemann_opt.cpp
@@ -158,65 +158,6 @@ static void Print(__m512 v)
 /// \param[in] pr - right side pressure
 /// \param[in] cr - right side sound speed
 /// \param[out] pm - pressure
-static void guessp(float dl, float ul, float pl, float cl,
-                   float dr, float ur, float pr, float cr,
-                   float &pm)
-{
-    float cup, gel, ger, pmax, pmin, ppv, pq, ptl, ptr, qmax, quser, um;
-
-    quser = 2.0;
-
-    // Compute guess pressure from PVRS Riemann solver.
-    cup = 0.25 * (dl + dr) * (cl + cr);
-    ppv = 0.5 * (pl + pr) + 0.5 * (ul - ur) * cup;
-    ppv = (ppv > 0.0) ? ppv : 0.0;
-    pmin = (pl < pr) ? pl : pr;
-    pmax = (pl > pr) ? pl : pr;
-    qmax = pmax / pmin;
-
-    if ((qmax <= quser) && (pmin <= ppv) && (ppv <= pmax))
-    {
-        // Select PVRS Riemann solver.
-        pm = ppv;
-    }
-    else
-    {
-        if (ppv < pmin)
-        {
-            // Select Two-Rarefaction Riemann solver.
-            pq = pow(pl / pr, G1);
-            um = (pq * ul / cl + ur / cr + G4 * (pq - 1.0)) / (pq / cl + 1.0 / cr);
-            ptl = 1.0 + G7 * (ul - um) / cl;
-            ptr = 1.0 + G7 * (um - ur) / cr;
-            pm = 0.5 * (pow(pl * ptl, G3) + pow(pr * ptr, G3));
-        }
-        else
-        {
-            // Select Two-Shock Riemann solver with PVRS as estimate.
-            gel = sqrt((G5 / dl) / (G6 * pl + ppv));
-            ger = sqrt((G5 / dr) / (G6 * pr + ppv));
-            pm = (gel * pl + ger * pr - (ur - ul)) / (gel + ger);
-        }
-    }
-}
-
-/// \brief 
-///
-/// Purpose is to provide a guessed value for pressure
-/// pm in the Star Region. The choice is made
-/// according to adaptive Riemann solver using
-/// the PVRS, TRRS and TSRS approximate
-/// Riemann solvers. See Sect. 9.5 of Chapt. 9 of Ref. 1.
-///
-/// \param[in] dl - left side density
-/// \param[in] ul - left side velocity
-/// \param[in] pl - left side pressure
-/// \param[in] cl - left side sound speed
-/// \param[in] dr - right side density
-/// \param[in] ur - right side velocity
-/// \param[in] pr - right side pressure
-/// \param[in] cr - right side sound speed
-/// \param[out] pm - pressure
 static void guessp_16(__m512 dl, __m512 ul, __m512 pl, __m512 cl,
                       __m512 dr, __m512 ur, __m512 pr, __m512 cr,
                       __m512 *pm)
